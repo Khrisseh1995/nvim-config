e522b35d391974e386467a648fe61fe02a92c0b1
Chris Clark <chris.clark876@gmail.com>
2021-12-29 21:22:41 +0000

    plugins and specific plugin config

---
+++ lua/plugin-settings/lightline.lua
@@ -0,0 +1,6 @@
+--Set statusbar
+vim.g.lightline = {
+  colorscheme = 'nord',
+  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
+  component_function = { gitbranch = 'fugitive#head' },
+}
