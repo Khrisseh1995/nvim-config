c8254d55e7a5cddd54b98f56c919635a41b44a3c
Khrisseh1995 <chris.clark876@gmail.com>
2022-03-21 02:55:38 +0000

    nvim tree options update

--- lua/plugin-settings/nvimtree.lua
+++ lua/plugin-settings/nvimtree.lua
@@ -5,8 +5,12 @@ local g = vim.g
 vim.o.termguicolors = true
 
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+g.nvim_tree_git_hl = git_status
+g.nvim_tree_gitignore = 0
+g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 --
@@ -63,6 +67,10 @@ nvimtree.setup {
       update_cwd = false,
    },
    view = {
+     winopts = {
+       relativenumber = true,
+       number = true,
+     },
       allow_resize = true,
       side = "left",
       width = 40,
